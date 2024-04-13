54952c5423bcb9ea71dbc59dbef469989a098921
DevrajDC <devrajchatribin9978@gmail.com>
2024-04-14 03:25:57 +0530

    Sign In screen UI completed

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
 import { Image, ScrollView, Text, View } from "react-native";
-import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
 
@@ -41,7 +42,7 @@ export default function App() {
 
           <CustomButton
             title="Continue with Email"
-            handlePress={() => {}}
+            handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
         </View>
