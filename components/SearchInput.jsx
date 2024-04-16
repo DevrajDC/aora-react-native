ce4607f336e3650c75e7b9219d43920c8d91d5fd
DevrajDC <devrajchatribin9978@gmail.com>
2024-04-17 00:47:40 +0530

    Implemented Search functionality

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -4,37 +4,28 @@ import { View, TouchableOpacity, Image, TextInput, Alert } from "react-native";
 
 import { icons } from "../constants";
 
-const SearchInput = ({
-  title,
-  value,
-  placeholder,
-  handleChangeText,
-  otherStyles,
-  ...props
-}) => {
+const SearchInput = ({ initialQuery }) => {
   const pathname = usePathname();
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState(initialQuery || "");
 
   return (
-    <View className="w-full h-16 px-4 bg-black-100 border-2 border-black-200 rounded-2xl focus:border-secondary items-center flex-row space-x-4">
+    <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary">
       <TextInput
         className="text-base mt-0.5 text-white flex-1 font-pregular"
         value={query}
-        placeholder="Search for a video topic"
+        placeholder="Search a video topic"
         placeholderTextColor="#CDCDE0"
         onChangeText={(e) => setQuery(e)}
       />
 
       <TouchableOpacity
         onPress={() => {
-          if (!query) {
+          if (query === "")
             return Alert.alert(
               "Missing Query",
               "Please input something to search results across database"
             );
-          }
 
-          // check if user is already on search page and then set search params
           if (pathname.startsWith("/search")) router.setParams({ query });
           else router.push(`/search/${query}`);
         }}
